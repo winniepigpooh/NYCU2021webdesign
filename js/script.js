c091698b222a0fcb6c29602141860238efb7fe61
winniepigpooh <winnie8931@gmail.com>
2021-03-23 15:00:04 +0800

    new directory

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+    document.getElementById("byid").innerHTML="我來自id";
+    document.getElementById("byid").style.color="blue";
+
+    document.getElementsByClassName("byClass")[0].innerHTML="我來自class";
+    document.getElementsByClassName("byClass")[0].style.fontSize="30px";
+
+
+    document.getElementsByTagName("p")[0].innerHTML="aabbccc";
+    document.getElementsByTagName("p")[0].style.color="red";
+    document.getElementsByTagName("p")[0].style.fontSize="30px";
+    
