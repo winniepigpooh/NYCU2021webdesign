0fcbe4225a3c594b290a910c0cc4fd65553a9da5
winniepigpooh <79505421+winniepigpooh@users.noreply.github.com>
2021-04-21 06:41:38 +0800

    midterm

--- js/script.js
+++ js/script.js
@@ -1,13 +1,8 @@
 
+$('.grid').masonry({
 
-    document.getElementById("byid").innerHTML="我來自id";
-    document.getElementById("byid").style.color="blue";
-
-    document.getElementsByClassName("byClass")[0].innerHTML="我來自class";
-    document.getElementsByClassName("byClass")[0].style.fontSize="30px";
-
-
-    document.getElementsByTagName("p")[0].innerHTML="aabbccc";
-    document.getElementsByTagName("p")[0].style.color="red";
-    document.getElementsByTagName("p")[0].style.fontSize="30px";
-    
+    itemSelector: '.grid-item',
+   
+     columnWidth: 320
+   
+   });
